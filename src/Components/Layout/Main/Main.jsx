4c54a306da25e0498434fe09775ecc046b0871fd
Juan Miguel <88735626+juanmiguelosorioarroyave@users.noreply.github.com>
2021-12-10 23:03:20 -0500

    Subiendo proyecto

---
+++ src/Components/Layout/Main/Main.jsx
@@ -0,0 +1,57 @@
+
+import { useState, useEffect } from 'react'
+import Logo from '../../../Images/fondo.jpg'
+import { Filtrar } from '../../UI/UIButton/UIButton'
+import { Card } from '../../UI/Card/Card' 
+
+function App() {
+	const [personajes, setPersonajes] = useState([])
+	const [loading, setLoading] = useState(true)
+	const [filter, setFilter] = useState('')
+
+	useEffect(() => {
+		const getPersonajes = async () => {
+			try {
+				const response = await fetch(
+					'https://rickandmortyapi.com/api/character'
+				)
+				const data = await response.json()
+				setPersonajes(data.results)
+				setLoading(false)
+			} catch (error) {
+				console.log(error)
+			}
+		}
+		getPersonajes()
+	}, [])
+
+	const personjesFiltrados = personajes.filter((personaje) =>
+		personaje.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase())
+	)
+
+	return (
+		<div className='container'>
+			<figure className='logo'>
+				<img className='fondo' src={Logo} alt='Logo de RickMorty' />
+			</figure>
+			<Filtrar filter={filter} setFilter={setFilter} />
+			<section className='lista-personajes'>
+				{loading ? (
+					<p>Cargando...</p>
+				) : personjesFiltrados.length > 0 ? (
+					personjesFiltrados.map((personaje) => (
+						<Card key={personaje.id} personaje={personaje} />
+					))
+				) : (
+					<p>
+						No se encontro personajes con la busqueda{' '}
+						<strong>"{filter}"</strong>.
+					</p>
+				)}
+			</section>
+		</div>
+	)
+}
+
+export default App
+
---
+++ src/Components/Page/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import App from "../../Layout/Main/Main";
+
+export const Home =() =>{
+    return(
+        <>
+            <App/>     
+        </>
+    );
+}
